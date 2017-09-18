367b98919fd6fabd97e88e0fc114b416e6e2ee89
Jelle van der Waa <j.vanderwaa@kopano.com>
2017-09-18 17:24:55 +0200

    Initial commit of basic Kopano WebApp plugin generator
    Generates a bare Kopano WebApp JavaScript plugin to easily start
    developping a plugin.

---
+++ generators/index.js
@@ -0,0 +1,59 @@
+const mkdirp = require("mkdirp");
+
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }, {
+      type    : 'input',
+      name    : 'author',
+      message : 'Author',
+      default : 'John Doe'
+    }, {
+      type    : 'input',
+      name    : 'url',
+      message : 'project URL',
+      default : 'https://kopano.com'
+    }, {
+      type    : 'input',
+      name    : 'description',
+      message : 'project URL',
+      default : 'My first plugin'
+    }]).then((answers) => {
+      this.log('app name', answers.name);
+      this.log('author', answers.author);
+      mkdirp.sync('js');
+      this.fs.copyTpl(
+        this.templatePath('_manifest.xml'),
+         this.destinationPath('manifest.xml'),
+         {
+           name: answers.name,
+           author: answers.author,
+           url: answers.url,
+           description: answers.description,
+	 }
+      ); 
+
+      this.fs.copyTpl(
+        this.templatePath('_plugin.js'),
+         this.destinationPath('js/' + answers.name + '.js'),
+         { 
+           name: answers.name,
+           classname: 'Plugin' + answers.name,
+	 }
+      ); 
+
+      this.fs.copyTpl(
+        this.templatePath('_build.xml'),
+         this.destinationPath('build.xml'),
+         {
+	 }
+      ); 
+    });
+  }
+};
---
+++ generators/templates/_plugin.js
@@ -0,0 +1,23 @@
+Ext.namespace('Zarafa.plugins.<%= name %>');
+
+/**
+ * @class Zarafa.plugins.<%= name %>.<%= classname %>
+ * @extends Zarafa.core.Plugin
+ */
+Zarafa.plugins.<%= name %>.%<%= classname %> = Ext.extend(Zarafa.core.Plugin, {
+	/**
+	 * @protected
+	 */
+	initPlugin : function()
+	{
+		Zarafa.plugins.<%= name %>.<%= classname %>.superclass.initPlugin.apply(this, arguments);
+	},
+});
+
+Zarafa.onReady(function() {
+	container.registerPlugin(new Zarafa.core.PluginMetaData({
+		name : '<%= name %>',
+		displayName : _('<%= name %>'),
+		pluginConstructor : Zarafa.plugins.<%= name %>.<%= classname %>
+	}));
+});
